2f94bb5cccd1dfc22e0754563f8b394160db34fa
chaimaby <chaima.benyounes1@gmail.com>
2019-02-22 15:40:42 +0100

    [add] replace homeCompenent with Albums.component

--- src/app/albums/albums.component.ts
+++ src/app/albums/albums.component.ts
@@ -2,25 +2,35 @@ import { Component, OnInit } from '@angular/core';
 // importez la définition de la classe et les albums
 import { Album } from '../album'; 
 import { ALBUMS } from '../mock-albums';
+import { AlbumService } from '../service/album.service';
 
 @Component({
   selector: 'app-albums',
   templateUrl: './albums.component.html',
   styleUrls: ['./albums.component.scss']
 })
-
 export class AlbumsComponent implements OnInit {
 
-  //albums : Album[] =  ALBUMS;
-  //status: string = null;// pour gérer l'affichage des caractères [play] 
-
-  constructor() { 
-
+  titlePage: string = "Page principale Albums Music";
+  albums : Album[] =  ALBUMS;
+  selectedAlbum : Album;
+  status: string = null; // pour gérer l'affichage des caractères [play]
+  
+  constructor(private ablumService: AlbumService) {
   }
 
   ngOnInit() {
+    //this.albums = this.ablumService.paginate(0,5);
+    console.log(this.ablumService.count())
+    this.albums = this.ablumService.getAlbums();
+  }
 
-    //console.log(this.albums[0].ref);
+  onSelect(album: Album) {
+    this.selectedAlbum = album;
+  }
+
+  playParent($event){
+    this.status = $event.id; // identifiant unique
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,15 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { AlbumsComponent } from './albums/albums.component';
 import { MinuteSecondsPipe } from './pipe/minute-seconds-pipe';
 import { AlbumDetailsComponent } from './album-details/album-details.component';
 
-
-const routeshome: Routes = [{ path:'', component : HomeComponent}] 
-//const routesAlbums: Routes = [{ path:'', component : AlbumsComponent}] 
+const routeshome: Routes = [{ path:'', component : AlbumsComponent}] 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     AlbumsComponent,
     MinuteSecondsPipe,
     AlbumDetailsComponent
