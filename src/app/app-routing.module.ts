d337e95dfb0be0ab164a47c07454db02d9fd8054
chaimaby <chaima.benyounes1@gmail.com>
2019-02-26 11:29:29 +0100

    [add]move the route from app.module.ts  to app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AlbumsComponent } from './albums/albums.component';
+import { LoginComponent } from './login/login.component';
+import { AlbumDescriptionComponent } from './album-description/album-description.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path:'albums', component : AlbumsComponent},
+  { path:'', redirectTo: '/albums', pathMatch:'full'},
+  { path:'login', component : LoginComponent},
+  { path:'register', component : RegisterComponent},
+  { path:'album/:id', component : AlbumDescriptionComponent},
+] 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule} from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AlbumsComponent } from './albums/albums.component';
 import { MinuteSecondsPipe } from './pipe/minute-seconds-pipe';
-import { AlbumDetailsComponent } from './album-details/album-details.component';
 import { SearchComponent } from './search/search.component';
+import { AlbumDetailsComponent } from './album-details/album-details.component';
+import { AlbumsComponent } from './albums/albums.component';
 import { LoginComponent } from './login/login.component';
 import { AlbumDescriptionComponent } from './album-description/album-description.component';
 import { RegisterComponent } from './register/register.component';
-
-const albumsRoutes: Routes = [
-  { path:'albums', component : AlbumsComponent},
-  { path:'', redirectTo: '/albums', pathMatch:'full'},
-  { path:'login', component : LoginComponent},
-  { path:'register', component : RegisterComponent},
-  { path:'album/:id', component : AlbumDescriptionComponent},
-] 
 
 @NgModule({
   declarations: [
@@ -36,9 +28,7 @@ const albumsRoutes: Routes = [
     BrowserModule,
     AppRoutingModule, 
     FormsModule,
-    RouterModule.forRoot(
-      albumsRoutes, 
-  )
+    RouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
