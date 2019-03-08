737e4d4cc7981c9e22a3cc735242134ae6fbc27b
chaimaby <chaima.benyounes1@gmail.com>
2019-03-08 10:26:02 +0100

    [add] routesModule in adminModule

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
+import { Routes,RouterModule} from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { AlbumComponent } from './album/album.component';
 import { ShareModule } from '../share/share.module';
+import { AlbumComponent } from './album/album.component';
+import { AddAlbumComponent  } from './../share/add-album/add-album.component';
+import { GuardService } from './../service/guard.service';
 
+const routes: Routes = [
+  { path:'admin/album', canActivate: [GuardService], component : AlbumComponent},
+  { path:'admin/add', canActivate: [GuardService], component : AddAlbumComponent },
+]
 @NgModule({
-  declarations: [AlbumComponent], // déclarer le component dans le module
+  declarations: [
+    AlbumComponent,// déclarer le component dans le module
+  ], 
   imports: [
     CommonModule, // directives classiques à importer dans le module
-    ShareModule
+    ShareModule,
+    RouterModule.forChild(routes) // définition des routes dans le sous-module
   ],
-  exports : [AlbumComponent]// exporter le component pour le reste de l'application
+  exports : [
+    AlbumComponent, // exporter le component pour le reste de l'application
+  ]
 })
 export class AdminModule { }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,6 @@ import { AlbumsComponent } from './albums/albums.component';
 import { LoginComponent } from './login/login.component';
 import { AlbumDescriptionComponent } from './album-description/album-description.component';
 import { RegisterComponent } from './register/register.component';
-import { AlbumComponent } from './admin/album/album.component';
-import { GuardService } from './service/guard.service';
 
 const routes: Routes = [
   { path:'albums', component : AlbumsComponent},
@@ -13,7 +11,6 @@ const routes: Routes = [
   { path:'login', component : LoginComponent},
   { path:'register', component : RegisterComponent},
   { path:'album/:id', component : AlbumDescriptionComponent},
-  { path:'admin/album', canActivate: [GuardService], component : AlbumComponent},
 ] 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
