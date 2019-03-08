33eca10f657a51dbe9c74b10ca3dae34c02d6a72
chaimaby <chaima.benyounes1@gmail.com>
2019-03-08 10:03:17 +0100

    [add] new component addAlbum in AdminModule

--- src/app/share/share.module.ts
+++ src/app/share/share.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PaginateComponent } from './paginate/paginate.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { PaginateComponent } from './paginate/paginate.component';
+import { AddAlbumComponent } from './add-album/add-album.component';
+
 @NgModule({
-  declarations: [PaginateComponent],
+  declarations: [
+    PaginateComponent,
+    AddAlbumComponent,
+  ],
   imports: [
     CommonModule
   ],
